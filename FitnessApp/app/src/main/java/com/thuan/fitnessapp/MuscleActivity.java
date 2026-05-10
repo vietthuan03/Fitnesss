48903d3992e7ac34840b0a0a755a720eee97c3be
vietthuan03 <vietthuan954@gmail.com>
2026-05-10 09:43:28 +0700

    Rename project to FitnessApp and update package name to com.thuan.fitnessapp

---
+++ FitnessApp/app/src/main/java/com/thuan/fitnessapp/MuscleActivity.java
@@ -0,0 +1,46 @@
+package com.thuan.fitnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.Fragment;
+
+import com.thuan.fitnessapp.data.models.Days;
+import com.thuan.fitnessapp.data.models.Workout;
+import com.thuan.fitnessapp.fragments.FragAMuscle;
+
+
+public class MuscleActivity extends BaseActivity {
+
+    Days day;
+    public Workout workout;
+    public int dayNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(getResources().getString(R.string.Fitness));
+        workout = getIntent().getExtras().getParcelable("currentWorkout");
+        dayNum = getIntent().getExtras().getInt("currentDay");
+        FragAMuscle simpleA = FragAMuscle.newInstance();
+        setContentView(R.layout.activity_muscle);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.content, simpleA)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment f = MuscleActivity.this.getSupportFragmentManager().findFragmentById(R.id.content);
+        if (f instanceof FragAMuscle) {
+            Intent intent = new Intent();
+            intent.putExtra("currentWorkout", workout);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+        super.onBackPressed();
+    }
+}
